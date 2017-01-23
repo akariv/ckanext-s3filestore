eafbff536dd56a7fa723ac78e4d400bfa23fec97
tino097 <konstantin.sivakov@gmail.com>
2017-01-23 21:48:12 +0100

    fix for access

--- ckanext/s3filestore/uploader.py
+++ ckanext/s3filestore/uploader.py
@@ -97,7 +97,7 @@ class BaseS3Uploader(object):
             # bucket_acl = s3.BucketAcl(self.bucket.name)
             # bucket_acl.Acl().put(ACL='public-read')
             #self.bucket.upload_file(upload_file, filepath)
-            s3.Object(self.bucket.name, filepath).put(Body=upload_file.read())
+            obj =  s3.Object(self.bucket.name, filepath).put(Body=upload_file.read())
             print 'Upload'
             obj.Acl().put(ACL='public-read')
             #s3.Object(self.bucket.name, upload_file).put(Body=open(filepath), 'rb')
